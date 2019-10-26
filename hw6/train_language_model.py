ea5b59c9993e3a77c0edf417e0734b055aa464ed
Bo Chen <bochen1993cs@gmail.com>
2019-10-26 16:45:56 -0500

    genreview

--- hw6/train_language_model.py
+++ hw6/train_language_model.py
@@ -109,6 +109,14 @@ for epoch in range(no_of_epochs):
 		loss, pred = model(x_input)
 		loss.backward()
 
+		if(epoch>6):
+			for group in optimizer.param_groups:
+				for p in group['params']:
+					state = optimizer.state[p]
+					if 'step' in state.keys():
+						if(state['step']>=1024):
+							state['step'] = 1000
+
 		norm = nn.utils.clip_grad_norm_(model.parameters(),2.0)
 
 		optimizer.step()   # update gradients
