24934f49972ca0dfc57b9da14f196b64ac74c1cf
boc2 <boc2@illinois.edu>
2019-11-02 15:30:13 -0500

    hw7

--- hw7/max_class.py
+++ hw7/max_class.py
@@ -31,6 +31,64 @@ def plot(samples):
         plt.imshow(sample)
     return fig
 
+no_of_hidden_units = 128
+class discriminator(nn.Module):
+    def __init__(self):
+        super(discriminator, self).__init__()
+        self.conv1 = nn.Conv2d(3, no_of_hidden_units, kernel_size=3, stride=1, padding=1)
+        self.ln1 = nn.LayerNorm([no_of_hidden_units,32,32])
+        self.lrelu1 = nn.LeakyReLU()
+
+        self.conv2 = nn.Conv2d(no_of_hidden_units, no_of_hidden_units, kernel_size=3, stride=2, padding=1)
+        self.ln2 = nn.LayerNorm([no_of_hidden_units,16,16])
+        self.lrelu2 = nn.LeakyReLU()
+
+        self.conv3 = nn.Conv2d(no_of_hidden_units, no_of_hidden_units, kernel_size=3, stride=1, padding=1)
+        self.ln3 = nn.LayerNorm([no_of_hidden_units,16,16])
+        self.lrelu3 = nn.LeakyReLU()
+
+        self.conv4 = nn.Conv2d(no_of_hidden_units, no_of_hidden_units, kernel_size=3, stride=2, padding=1)
+        self.ln4 = nn.LayerNorm([no_of_hidden_units,8,8])
+        self.lrelu4 = nn.LeakyReLU()
+
+        self.conv5 = nn.Conv2d(no_of_hidden_units, no_of_hidden_units, kernel_size=3, stride=1, padding=1)
+        self.ln5 = nn.LayerNorm([no_of_hidden_units,8,8])
+        self.lrelu5 = nn.LeakyReLU()
+
+        self.conv6 = nn.Conv2d(no_of_hidden_units, no_of_hidden_units, kernel_size=3, stride=1, padding=1)
+        self.ln6 = nn.LayerNorm([no_of_hidden_units,8,8])
+        self.lrelu6 = nn.LeakyReLU()
+
+        self.conv7 = nn.Conv2d(no_of_hidden_units, no_of_hidden_units, kernel_size=3, stride=1, padding=1)
+        self.ln7 = nn.LayerNorm([no_of_hidden_units,8,8])
+        self.lrelu7 = nn.LeakyReLU()
+
+        self.conv8 = nn.Conv2d(no_of_hidden_units, no_of_hidden_units, kernel_size=3, stride=2, padding=1)
+        self.ln8 = nn.LayerNorm([no_of_hidden_units,4,4])
+        self.lrelu8 = nn.LeakyReLU()
+
+        self.pool = nn.MaxPool2d(4, 4)
+        self.fc1 = nn.Linear(no_of_hidden_units, 1)
+        self.fc10 = nn.Linear(no_of_hidden_units, 10)
+
+    def forward(self, x, extract_features=0):
+        x = self.ln1(self.lrelu1(self.conv1(x)))
+        x = self.ln2(self.lrelu2(self.conv2(x)))
+        x = self.ln3(self.lrelu3(self.conv3(x)))
+        x = self.ln4(self.lrelu4(self.conv4(x)))
+        x = self.ln5(self.lrelu5(self.conv5(x)))
+        x = self.ln6(self.lrelu6(self.conv6(x)))
+        x = self.ln7(self.lrelu7(self.conv7(x)))
+        x = self.ln8(self.lrelu8(self.conv8(x)))
+        # if(extract_features==8):
+        #     h = F.max_pool2d(x,4,4)
+        #     h = h.view(-1, no_of_hidden_units)
+        #     return h
+        x = self.pool(x)
+        x = x.view(-1, no_of_hidden_units)
+        y1,y2 = self.fc1(x),self.fc10(x)
+        return y1,y2
+        
 import os
 if not os.path.exists('visualization'):
     os.mkdir('visualization')
